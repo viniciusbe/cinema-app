b5b13a095f9cd393dd95bf4b11bb851a3c5135ce
Vinícius Bernardes <vinicius@bernardes.dev>
2023-11-03 03:41:11 -0300

    feat: create default layout file

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-between p-8">
+    <h1 class="text-xl">Filmes</h1>
+    <div class="flex flex-row gap-2">
+      <UButton variant="soft" to="teste">Login</UButton>
+      <UButton variant="ghost">Cadastro</UButton>
+    </div>
+  </div>
+  <UDivider class="mb-8" />
+  <NuxtPage />
+</template>
